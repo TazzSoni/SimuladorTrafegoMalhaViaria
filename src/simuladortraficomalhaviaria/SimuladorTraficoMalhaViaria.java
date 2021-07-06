aec1b5d37e5779e1d14bc88d64951b2c21610ba0
Rodrigo <tazzsoni@gmail.com>
2021-07-06 18:00:05 -0300

    Início Projeto

---
+++ src/simuladortraficomalhaviaria/SimuladorTraficoMalhaViaria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladortraficomalhaviaria;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class SimuladorTraficoMalhaViaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
